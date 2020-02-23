46808e010fd00ac1a65b7f23835b47e99e5dda58
bimonestle <bimonestle@yahoo.co.id>
2020-02-23 13:32:58 +0700

    Parse links from response.Body. Add Request URL. Add Base URL

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
+	"net/url"
+
+	"github.com/bimonestle/go-exercise-projects/04.HTML-Link-Parser/link"
 )
 
 // 1. GET the webpage.
@@ -16,10 +17,10 @@ import (
 // 5. Find all pages (BFS)
 // 6. Print out XML
 func main() {
-	urlFlag := flag.String("url", "https://gophercises.com", "The url that you want to build a sitemap for")
+	urlFlag := flag.String("url", "https://gophercises.com/doremi/fasol/", "The url that you want to build a sitemap for")
 	flag.Parse()
 
-	fmt.Println(*urlFlag)
+	fmt.Println("The urlFlag: ", *urlFlag)
 
 	// 1. GET the webpage.
 	resp, err := http.Get(*urlFlag)
@@ -28,5 +29,31 @@ func main() {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+	// io.Copy(os.Stdout, resp.Body) // for testing purpose
+
+	// 2. Parse all the links from an url
+	links, _ := link.Parse(resp.Body)
+	for _, l := range links {
+		fmt.Println("The parsed link from a response Body: ", l)
+	}
+
+	// Get the URL from a requested URL
+	// Example:
+	// 		URL: "https://abc.com/efg/"
+	// 		Requested URL: "https://abc.com/efg/"
+	reqURL := resp.Request.URL
+	fmt.Println("Request URL: ", reqURL.String())
+
+	// Get the base form of URL from a URL
+	// Example:
+	// 		URL : "https://abc.com/efg/hi"
+	// 		Requested URL: "https://abc.com/efg/hi"
+	// 		Base URL: "https://abc.com"
+	baseURL := &url.URL{
+		Scheme: reqURL.Scheme, // "https:""
+		Host:   reqURL.Host,   // "//some-domain.com"
+	}
+	base := baseURL.String()
+	fmt.Println("Base URL: ", base)
+
 }
